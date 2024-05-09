d83209e3fe52a259a5d5977cc46acaae330bbafd
Miles Smith <miles-smith@omrf.org>
2024-05-09 13:55:06 -0500

    replace numpy with jax.numpy

--- src/pyalra/alra.py
+++ src/pyalra/alra.py
@@ -1,5 +1,6 @@
-import numpy as np
-import numpy.typing as npt
+from jax import jit
+import jax.numpy as jnp
+import jax.typing as jt
 import scipy as sp
 from loguru import logger
 from sklearn.utils.extmath import randomized_svd
@@ -7,9 +8,9 @@ from sklearn.utils.extmath import randomized_svd
 from pyalra.choose_k import choose_k
 from pyalra.logging import init_logger
 
-
+@jit
 def alra(
-    a_norm: npt.ArrayLike | sp.sparse.spmatrix,
+    a_norm: jt.ArrayLike | sp.sparse.spmatrix,
     k: int = 0,
     q: int = 10,
     quantile_prob: float = 0.001,
@@ -18,16 +19,16 @@ def alra(
     *,
     save_log: bool = False,
     log_level: int = 1,
-) -> dict[str, npt.ArrayLike]:
+) -> dict[str, jt.ArrayLike]:
     if debug:
         init_logger(log_level, save_log)
 
     logger.info(f"Read matrix with {a_norm.shape[0]} cells and {a_norm.shape[1]} genes")
 
-    if isinstance(a_norm, np.matrix):
-        a_norm = a_norm.A
-    elif isinstance(a_norm, sp.sparse.spmatrix):
-        a_norm = a_norm.todense().A
+    # if isinstance(a_norm, np.matrix):
+    #     a_norm = a_norm.A
+    # elif isinstance(a_norm, sp.sparse.spmatrix):
+    #     a_norm = a_norm.todense().A
 
     if k == 0:
         k, *_ = choose_k(a_norm)
@@ -39,34 +40,34 @@ def alra(
     logger.debug("Running rsvd")
     u, d, v = randomized_svd(M=a_norm, n_components=k, n_iter=q, random_state=seed)
 
-    a_norm_rank_k = np.matmul(np.matmul(u[:, :k], np.diag(d[:k])), v[:k, :])
+    a_norm_rank_k = jnp.matmul(jnp.matmul(u[:, :k], jnp.diag(d[:k])), v[:k, :])
 
     logger.info(f"Find the {quantile_prob} quantile for each gene")
-    a_norm_rank_k_mins = abs(np.quantile(a_norm_rank_k, axis=0, q=0.001))
+    a_norm_rank_k_mins = abs(jnp.quantile(a_norm_rank_k, axis=0, q=0.001))
 
     logger.info("Sweep")
     a_norm_rank_k_cor = a_norm_rank_k.copy()
-    a_norm_rank_k_cor[a_norm_rank_k <= np.tile(a_norm_rank_k_mins, (len(a_norm_rank_k), 1))] = 0
+    a_norm_rank_k_cor[a_norm_rank_k <= jnp.tile(a_norm_rank_k_mins, (len(a_norm_rank_k), 1))] = 0
 
     sigma_1 = sp.stats.tstd(a_norm_rank_k_cor, axis=0)
     sigma_2 = sp.stats.tstd(a_norm, axis=0)
-    mu_1 = np.divide(np.sum(a_norm_rank_k_cor, axis=0), np.sum(a_norm_rank_k_cor > 0, axis=0))
-    mu_2 = np.divide(np.sum(a_norm, axis=0), np.sum(a_norm > 0, axis=0))
+    mu_1 = jnp.divide(jnp.sum(a_norm_rank_k_cor, axis=0), jnp.sum(a_norm_rank_k_cor > 0, axis=0))
+    mu_2 = jnp.divide(jnp.sum(a_norm, axis=0), jnp.sum(a_norm > 0, axis=0))
 
-    toscale = np.logical_and(
-        np.logical_and(~np.isnan(sigma_1), ~np.isnan(sigma_2)),
-        np.logical_and(~np.logical_and((sigma_1 == 0), (sigma_2 == 0)), ~(sigma_1 == 0)),
+    toscale = jnp.logical_and(
+        jnp.logical_and(~jnp.isnan(sigma_1), ~jnp.isnan(sigma_2)),
+        jnp.logical_and(~jnp.logical_and((sigma_1 == 0), (sigma_2 == 0)), ~(sigma_1 == 0)),
     )
 
     logger.info(f"Scaling all except for {sum(~toscale)} columns")
 
-    sigma_1_2 = np.divide(sigma_2, sigma_1)
+    sigma_1_2 = jnp.divide(sigma_2, sigma_1)
 
-    toadd = np.add(-np.divide(np.multiply(mu_1, sigma_2), sigma_1), mu_2)
+    toadd = jnp.add(-jnp.divide(jnp.multiply(mu_1, sigma_2), sigma_1), mu_2)
 
     a_norm_rank_k_temp = a_norm_rank_k_cor[:, toscale].copy()
-    a_norm_rank_k_temp = np.multiply(a_norm_rank_k_temp, sigma_1_2[toscale])
-    a_norm_rank_k_temp = np.add(a_norm_rank_k_temp, toadd[toscale])
+    a_norm_rank_k_temp = jnp.multiply(a_norm_rank_k_temp, sigma_1_2[toscale])
+    a_norm_rank_k_temp = jnp.add(a_norm_rank_k_temp, toadd[toscale])
 
     a_norm_rank_k_cor_sc = a_norm_rank_k_cor.copy()
     a_norm_rank_k_cor_sc[:, toscale] = a_norm_rank_k_temp
@@ -77,14 +78,14 @@ def alra(
 
     a_norm_size = a_norm.shape[0] * a_norm.shape[1]
     logger.info(
-        f"{100*np.sum(lt0)/a_norm_size:.2f}% of the values became negative in the scaling process and were set to zero"
+        f"{100*jnp.sum(lt0)/a_norm_size:.2f}% of the values became negative in the scaling process and were set to zero"
     )
 
-    nonzero_mask = np.logical_and(originally_nonzero, (a_norm_rank_k_cor_sc == 0))
+    nonzero_mask = jnp.logical_and(originally_nonzero, (a_norm_rank_k_cor_sc == 0))
     a_norm_rank_k_cor_sc[nonzero_mask] = a_norm[nonzero_mask]
 
-    original_nz = np.sum(a_norm > 0) / a_norm_size
-    completed_nz = np.sum(a_norm_rank_k_cor_sc > 0) / a_norm_size
+    original_nz = jnp.sum(a_norm > 0) / a_norm_size
+    completed_nz = jnp.sum(a_norm_rank_k_cor_sc > 0) / a_norm_size
     logger.info(f"The matrix went from {100*original_nz:.2f}% nonzero to {100*completed_nz:.2f}% nonzero")
 
     return {
--- src/pyalra/choose_k.py
+++ src/pyalra/choose_k.py
@@ -1,35 +1,36 @@
 from statistics import mean, stdev
 
-import numpy as np
-import numpy.typing as npt
+from jax import jit
+import jax.numpy as jnp
+import jax.typing as jt
 from sklearn.utils.extmath import randomized_svd
 
-
+@jit
 def choose_k(
-    A_norm: npt.ArrayLike,
+    A_norm: jt.ArrayLike,
     K: int = 100,
     thresh: int = 6,
     noise_start: int = 80,
     q: int = 2,
     **kwargs,
-) -> tuple[int, npt.ArrayLike, npt.ArrayLike]:
-    if K > np.min(A_norm.shape):
+) -> tuple[int, jt.ArrayLike, jt.ArrayLike]:
+    if K > jnp.min(A_norm.shape):
         msg = "For an m by n matrix, K must be smaller than the min(m,n)."
         raise ValueError(msg)
     if noise_start > (K - 5):
         msg = "There need to be at least 5 singular values considered noise."
         raise ValueError(msg)
 
-    noise_svals = list(range(noise_start - 1, K))
+    noise_svals = jnp.array(range(noise_start - 1, K))
     seed = kwargs.get("seed", None)
     _, d, _ = randomized_svd(M=A_norm, n_components=K, n_iter=q, random_state=seed)
 
     diffs = d[:-1] - d[1:]
 
-    mu = mean(diffs[np.subtract(noise_svals, 1)])
-    sigma = stdev(diffs[np.subtract(noise_svals, 1)])
+    mu = mean(diffs[jnp.subtract(noise_svals, 1)])
+    sigma = stdev(diffs[jnp.subtract(noise_svals, 1)])
     num_of_sds = (diffs - mu) / sigma
 
-    k = np.max(np.where(num_of_sds > thresh)) + 1
+    k = jnp.max(jnp.where(num_of_sds > thresh)[0]) + 1
 
-    return k, num_of_sds, d
+    return int(k), num_of_sds, d
