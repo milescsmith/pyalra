256d53043ac2bc09e27e16fff5513f6d35ad6116
Miles Smith <mileschristiansmith@gmail.com>
2024-07-04 11:49:55 -0500

    switch to PDM, split config files

--- noxfile.py
+++ noxfile.py
@@ -1,30 +1,33 @@
+import os
+
 import nox
-from nox_poetry import session
 
 package = "pyalra"
 # nox.options.sessions = ["lint", "black", "tests"]
 nox.options.sessions = ["tests"]
 locations = "src", "tests", "noxfile.py", "docs/conf.py"
+os.environ["PDM_IGNORE_SAVED_PYTHON"] = "1"
 
-@session(python="3.10")
-def tests(session: session) -> None:
+
+@nox.session(python="3.10")
+def tests(session: nox.session) -> None:
     # args = session.posargs or locations
     # install_with_constraints(session, ".")
     # install_with_constraints(session, "pytest")
-    session.run_always("poetry", "install", external=True)
+    session.run_always("pdm", "install", "-G", "test", external=True)
     session.run("pytest")
 
 
-@session()
-def black(session: session) -> None:
+@nox.session()
+def black(session: nox.session) -> None:
     """Run black code formatter."""
     session.install("black")
-    session.run_always("poetry", "install", external=True)
+    session.run_always("pdm", "install", "-G", "test", external=True)
     session.run("black", "src")
 
 
-@session
-def lint(session: session) -> None:
+@nox.session
+def lint(session: nox.session) -> None:
     """Lint using ruff."""
     args = session.posargs or locations
     session.install("ruff")
